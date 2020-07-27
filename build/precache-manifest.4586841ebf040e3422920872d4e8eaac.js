187e6df9edecaf5a8dcfe09c79b0f90a2cb67587
liam <liam.bracken@gmail.com>
2020-07-27 18:42:42 +0100

    uibuild

---
+++ build/precache-manifest.4586841ebf040e3422920872d4e8eaac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a05ac7342a6d9b3b20e5f27c68aac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e763654b339f34ce6b11",
+    "url": "/static/css/main.fb9df03f.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e763654b339f34ce6b11",
+    "url": "/static/js/main.240f8078.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
